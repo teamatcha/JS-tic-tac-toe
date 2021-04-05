81803ab8f2a5f9ff0fe334b518827bbe04297251
angela <p.angela9292@gmail.com>
2021-04-05 15:44:11 +0200

    restart button added

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,129 @@
 boxGrid = document.getElementById("grid-container");
+restartButton = document.getElementById("resetGame");
+
+const restartGame = () => {
+    restartButton.addEventListener("click", () => {
+        document.querySelectorAll(".cell-class").forEach((button) => {
+            button.innerHTML = "";
+            button.removeAttribute("disabled")
+            board = boardGame.board(9,"")
+        })
+    });
+}
+
 
 const boardGame = (() => {
     const board = (length, element) => Array.from({length}, () => element);
     
     const makeCell = (array) => {
         array.forEach(e => {
-            popo = document.createElement("button");
-            popo.classList.add("cell-class")
-            popo.innerHTML = e;
-            boxGrid.appendChild(popo)
+            button = document.createElement("button");
+            button.classList.add("cell-class")
+            button.innerHTML = e;
+            boxGrid.appendChild(button)
         })
-     //   console.log(array)
     }
 
     return{
         board,
         makeCell
     };
+
 })();
+
+const players = (name) => {
+    const player = () => console.log(`are you a pirate or marine ?`)
+
+    return {player};
+}
+
+        
+const placeMaker = (name) => {
+    const {player} = players(name)
+
+    var marker = 1;
+    const toto = () => {
+        document.querySelectorAll(".cell-class").forEach((button,index) => {
+            button.addEventListener('click', e =>{
+                if(marker === 1){
+                    button.innerHTML = "X"
+                    board[index] = button.innerHTML
+                    marker = 0
+
+                }else{
+                    button.innerHTML = "O"
+                    board[index] = button.innerHTML
+                    marker = 1
+                }
+                button.setAttribute("disabled", "disabled")
+                checkWin()
+              }) 
+            button.setAttribute('style', 'color : black;')    
+        })
+    }
+
+    const everyValue = (value) => value == "O" || value == "X";
+
+    const checkWin = () => {
+        winningCombo = [
+            [board[0],board[1],board[2]], 
+            [board[3],board[4],board[5]], 
+            [board[6],board[7],board[8]],
+            [board[0],board[3],board[6]], 
+            [board[1],board[4],board[7]], 
+            [board[2],board[5],board[8]],
+            [board[0],board[4],board[8]], 
+            [board[2],board[4],board[6]]
+        ];
+
+        for(var i=0; i< winningCombo.length; i++){
+            if (winningCombo[i].every(everyValue))
+            if(winningCombo[i][0] == winningCombo[i][1] && winningCombo[i][0] == winningCombo[i][2] ){
+                alert("win !")
+                document.querySelectorAll(".cell-class").forEach((button) => {
+                    button.setAttribute("disabled","disabled")
+                })
+
+            }
+        }
+        
+    }
+
+    return {player, toto, checkWin}
+}
+
+
+
+
+const play = () => {
+    //init board game
+    board = boardGame.board(9, "")
+
+    boardGame.makeCell(board);
+
+
+    //place maker
+    const luffy = placeMaker("luffy")
+    luffy.toto()
+
+    restartGame();
+
+};
+
+play();
+
+
+/*
+function test(){
+    document.querySelector("#grid-container").
+}*/
+
+/*
+const luffy = placeMaker('Luffy')
+luffy.toto();
+//test()
+console.log( board[0])
+*/
 
 /*board = boardGame.board(9,"-")
 board[0] = "yoyo"
@@ -37,92 +143,3 @@ console.log(titi)
 console.log(newnew.push(titi.splice(0,2)))
 
 console.log(newnew)*/
-
-
-const players = (name) => {
-    const player = () => console.log(`my name is ${name}`)
-    return {player};
-}
-
-const placeMaker = (name) => {
-    const {player} = players(name)
-
-    var momo = 1;
-    const toto = () => {
-        document.querySelectorAll(".cell-class").forEach((button,index) => {
-            button.addEventListener('click', e =>{
-                if(momo === 1){
-                    button.innerHTML = "X"
-                    board[index] = button.innerHTML
-                    momo = 0
-
-                }else{
-                    button.innerHTML = "O"
-                    board[index] = button.innerHTML
-                    momo = 1
-                }
-                button.setAttribute("disabled", "disabled")
-                checkWin()
-              }) 
-            button.setAttribute('style', 'color : black;')    
-
-            
-        })
-    }
-
-    const everyValue = (vaue) => vaue == "O" || vaue == "X";
-    const checkWin = () => {
-        rowWin = [[board[0],board[1],board[2]], [board[3],board[4],board[5]], [board[6],board[7], board[8]]]
-        columnWin =[[board[0],board[3],board[6]], [board[1],board[4],board[7]], [board[2],board[5], board[8]]]
-        diagonalWin = [[board[0], board[4], board[8]], [board[2], board[4], board[6]]]
-
-        for(var i=0; i< 3; i++){
-            if (rowWin[i].every(everyValue))
-                if(rowWin[i][0] === rowWin[i][1] && rowWin[i][0] === rowWin[i][2] ){
-                    alert("win !")
-                }
-
-            if (columnWin[i].every(everyValue))
-                if(columnWin[i][0] == columnWin[i][1] && columnWin[i][0] == columnWin[i][2] ){
-                    alert("win 2!")
-                }
-        }
-        for(var i=0; i< 2; i++){
-            if (diagonalWin[i].every(everyValue))
-            if(diagonalWin[i][0] == diagonalWin[i][1] && diagonalWin[i][0] == diagonalWin[i][2] ){
-                alert("win 3!")
-            }
-        }
-
-    }
-
-    return {player, toto, checkWin}
-}
-
-const play = () => {
-    //init board game
-    board = boardGame.board(9, "")
-
-    boardGame.makeCell(board);
-
-    //place maker
-    const luffy = placeMaker("luffy")
-    luffy.toto()
-
-    //luffy.lolo();
-};
-
-play();
-
-
-/*
-function test(){
-    document.querySelector("#grid-container").
-}*/
-
-/*
-const luffy = placeMaker('Luffy')
-luffy.toto();
-//test()
-console.log( board[0])
-*/
