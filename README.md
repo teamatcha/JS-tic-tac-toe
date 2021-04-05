79300030fa77a77ff9e4c9a8cd658de9256681f9
angela <p.angela9292@gmail.com>
2021-04-05 21:53:16 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Project JavaScript - Tic Tac Toe (from TOP)
+
+Used of HTML, CSS, JavaScript (concepts of DOM Manipulation, Factory Functions and Module Pattern)
+
+## What works
+Placing X or O marker on the board. 
+
+When a marker is placed, it can't no longer be modified.
+
+If there's a win, the game is stopped ie can't no longer placed anymore marker on the board.
+Alert of the win or draw.
+
+Button restart game => OK
+
+## Need to do
+Connect the two players to markers.
+
+Give the possibility of user to have input names.
+
+Give the possibility of playing against computer ?
--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,22 @@
 boxGrid = document.getElementById("grid-container");
 restartButton = document.getElementById("resetGame");
 
-const restartGame = () => {
-    restartButton.addEventListener("click", () => {
-        document.querySelectorAll(".cell-class").forEach((button) => {
-            button.innerHTML = "";
-            button.removeAttribute("disabled")
-            board = boardGame.board(9,"")
-        })
-    });
-}
+boxPlayer1 = document.getElementById("player1");
+/*
+innput = document.createElement("input");
+innput.setAttribute("placeholder","enter your name")
+buttonSubmit = document.createElement("button");
+buttonSubmit.innerHTML = "submit"
+innput.appendChild(buttonSubmit);
+
+boxPlayer1.appendChild(innput)
+boxPlayer1.appendChild(buttonSubmit)
+*/
 
 const boardGame = (() => {
     const board = (length, element) => Array.from({length}, () => element);
     
-    const makeCell = (array) => {
+    const createCell = (array) => {
         array.forEach(e => {
             button = document.createElement("button");
             button.classList.add("cell-class")
@@ -22,21 +24,25 @@ const boardGame = (() => {
             boxGrid.appendChild(button)
         })
     }
-
-    return{
-        board,
-        makeCell
-    };
-
+    return{board, createCell};
 })();
 
-const players = (name) => {
-    const player = () => console.log(`are you a pirate or marine ?`)
 
+const players = (name) => {
+    const player = () => console.log("test");
+    //player = document.getElementById("player1").innerHTML;
+    //console.log(fefe)
+    /*const getName = () => name;
+
+    const test = () => {
+        buttonSubmit.addEventListener("click", () => {
+            player.name = innput.value;
+            console.log(player.name)
+        })
+    }*/
     return {player};
 }
 
-        
 const placeMaker = (name) => {
     const {player} = players(name)
 
@@ -48,15 +54,14 @@ const placeMaker = (name) => {
                     button.innerHTML = "X"
                     board[index] = button.innerHTML
                     marker = 0
-
-                }else{
+                } else{
                     button.innerHTML = "O"
                     board[index] = button.innerHTML
                     marker = 1
                 }
                 button.setAttribute("disabled", "disabled")
                 checkWin()
-              }) 
+            }) 
             button.setAttribute('style', 'color : black;')    
         })
     }
@@ -75,7 +80,7 @@ const placeMaker = (name) => {
             [board[2],board[4],board[6]]
         ];
 
-        for(var i=0; i< winningCombo.length; i++){
+        for(let i=0; i< winningCombo.length; i++){
             if(winningCombo[i][0] == winningCombo[i][1] && winningCombo[i][0] == winningCombo[i][2] && winningCombo[i].every(everyValue)){
                 alert("win !")
                 document.querySelectorAll(".cell-class").forEach((button) => {
@@ -84,31 +89,44 @@ const placeMaker = (name) => {
                 true
             }
         }
-
         if(board.every(everyValue)){
             alert("draw !")
         }
-        
     }
 
     return {player, toto, checkWin}
 }
 
+const restartGame = () => {
+    restartButton.addEventListener("click", init);
+}
+
+function init(){
+    document.querySelectorAll(".cell-class").forEach((button) => {
+        button.innerHTML = "";
+        button.removeAttribute("disabled")
+        board = boardGame.board(9,"")
+    })
+}
 
 const play = () => {
     //init board game
     board = boardGame.board(9, "")
+    boardGame.createCell(board);
 
-    boardGame.makeCell(board);
-
-
-    //place maker
-    const luffy = placeMaker("luffy")
-    luffy.toto()
+    /*player1 = document.getElementById("player1").innerHTML;
+    player2 = document.getElementById("player2").innerHTML;
+    console.log(player1)
+    toto = players(player1);
+    console.log(toto)
+    */
+    const test = placeMaker(players.player)
+    test.toto()
 
     restartGame();
 
 };
+
 
 play();
 
